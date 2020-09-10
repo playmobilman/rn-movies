d7b3200d558ea73e50c0bfbbc729b9e3eec4ec53
Matias Schmid <playmobilman@gmail.com>
2020-09-10 00:44:28 -0300

    first commit

--- App.js
+++ App.js
@@ -5,8 +5,19 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      
+      <View style={styles.header}>
+        <Text>Header</Text>
+      </View>
+      
+      <View style={styles.main}>
+        <Text>Main</Text>
+      </View>
+      
+      <View style={styles.footer}>
+        <Text>Footer</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +25,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignContent: 'space-between',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  header: {
+    backgroundColor: '#afb6bd',
+    flexGrow: 1
+  },
+
+  main: {
+    backgroundColor: '#afb6bd',
+    flexGrow: 5,
+    backgroundColor: '#fff',
+  },
+
+  footer: {
+    backgroundColor: '#afb6bd',
+    flexGrow: 1
+  }
+
 });
